30d24d435b3c6b55e108389f941ec4074300ceeb
Armenuhi Yeghoyan <armine922@gmail.com>
2023-08-28 04:05:34 -0700

    FigurePainter

---
+++ src/homework/FigurePainter.java
@@ -0,0 +1,86 @@
+package homework;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        int height = 5;
+          //1
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+
+        }
+        System.out.println("-------------");
+
+
+            //2
+
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height - i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+        System.out.println("-------------");
+
+            //3
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < (height - i - 1); j++)
+                System.out.print("  ");
+            for (int j = 0; j <= i; j++)
+                System.out.print("* ");
+
+
+            System.out.println();
+        }
+        System.out.println("---------------");
+
+
+       //4
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < height - i; j++) {
+                System.out.print("* ");
+            }
+
+
+            System.out.println();
+
+
+        }
+
+
+        System.out.println("--------------");
+
+
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < (height - i - 1); j++)
+                System.out.print(" ");
+            for (int j = 0; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();}
+
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j <=i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j < height - i; j++) {
+                    System.out.print("* ");
+                }
+
+
+                System.out.println();
+
+
+            }
+
+
+    }
+    }
---
+++ src/homework/Task1.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class Task1 {
+
+public static void main(String[] args) {
+    for(int i=0;i<6;i++){
+        System.out.println("******");
+    }}}
