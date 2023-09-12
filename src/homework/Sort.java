89b8c6514679977bc1db0256b41ba23adf4f7469
Armenuhi Yeghoyan <armine922@gmail.com>
2023-09-12 13:21:21 -0700

    Sort

--- src/homework/Sort.java
+++ src/homework/Sort.java
@@ -25,27 +25,6 @@ public class Sort {
             }
         }
 
-        for (int i = 0; i < array.length; i++) {
-            int min = i;
-            for (int j = min + 1; j < array.length; j++) {
-                if (array[j] < array[min]) {
-                    min = j;
-
-                }
-            }
-            if (array[i] != array[min]) {
-
-                int tmp = array[i];
-                array[i] = array[min];
-                array[min] = tmp;
-                for (int y : array) {
-                    System.out.print(y + ",");
-
-                }
-                System.out.println();
-
-            }
-        }
 
     }
 }
