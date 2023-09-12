4825afa9408dfeac05f1739219497bbe2b18f7c6
Armenuhi Yeghoyan <armine922@gmail.com>
2023-09-12 13:17:51 -0700

    Sorting

---
+++ src/homework/Selection.java
@@ -0,0 +1,30 @@
+package homework;
+
+public class Selection {
+    public static void main(String[] args) {
+
+
+        int[] array = {4, 7, 1, 3, 9, 0, 2};
+
+    for (int i = 0; i < array.length; i++) {
+        int min = i;
+        for (int j = min + 1; j < array.length; j++) {
+            if (array[j] <array[min]) {
+                min = j;
+
+            }
+        }
+        if (array[i]!=array[min]) {
+
+            int tmp=array[i];
+            array[i]=array[min];
+            array[min]=tmp;
+            for(int y:array){
+                System.out.print(y+",");
+
+            }System.out.println();
+
+        }
+    }
+
+}}
--- src/homework/Sort.java
+++ src/homework/Sort.java
@@ -1,7 +1,7 @@
 package homework;
 
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-
+//Bubble
 public class Sort {
     public static void main(String[] args) {
         int[] array = {4, 7, 1, 3, 9, 0, 2};
@@ -23,5 +23,27 @@ public class Sort {
 
             }
         }
+
+        for (int i = 0; i < array.length; i++) {
+            int min = i;
+            for (int j = min + 1; j < array.length; j++) {
+                if (array[j] <array[min]) {
+                    min = j;
+
+                }
+            }
+            if (array[i]!=array[min]) {
+
+                int tmp=array[i];
+                array[i]=array[min];
+                array[min]=tmp;
+                for(int y:array){
+                    System.out.print(y+",");
+
+                }System.out.println();
+
+            }
+        }
+
     }
 }
